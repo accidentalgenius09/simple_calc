82c2c5b59222f5d53f29496c4f29b0d0dcd42b91
Surjith  K <surjithbhy007@gmail.com>
2023-01-14 13:10:45 +0530

    Initial push

---
+++ script.js
@@ -0,0 +1,17 @@
+function valenter(val){
+    result.value+=val
+}
+
+function allclr(){
+    result.value=''
+}
+
+function bckspace(){
+    result.value=result.value.slice(0,-1)
+    // data=result.value
+    // s=data.slice(0,-1)
+    //result.
+}
+function res(){
+    result.value=eval(result.value)
+}
